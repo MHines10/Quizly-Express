305b1040018b354dbb20e675bda9e338e3a82c5f
MHines10 <cd.mhines@gmail.com>
2023-01-30 16:12:31 -0500

    update register route and views folder location

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require('express');
-const { init } = require('express/lib/application');
 const app = express();
 const port = 3000;
+const path = require('path');
 
 app.set('view engine', 'ejs')
 
+// Update the location of the folder for res.render to use
+app.set('views', path.join(__dirname, 'src/templates/views'))
+
 app.get('/', (req, res) => {
-    res.render('register', {username: 'MontiH'});
+    res.send('Hello World');
 });
 
 // impor tthe function from routes module
 const initRoutes = require('./src/routes');
+
 // Execute the function with app as argumwnt
 initRoutes(app);
 
