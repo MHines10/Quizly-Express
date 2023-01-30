552b386cbd9f83276198754a524d5a41ff3de90f
MHines10 <cd.mhines@gmail.com>
2023-01-30 15:51:44 -0500

    add auth router

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { init } = require('express/lib/application');
 const app = express();
 const port = 3000;
 
@@ -8,6 +9,10 @@ app.get('/', (req, res) => {
     res.render('register', {username: 'MontiH'});
 });
 
+// impor tthe function from routes module
+const initRoutes = require('./src/routes');
+// Execute the function with app as argumwnt
+initRoutes(app);
 
 app.listen(port, () => {
     console.log(`server is listening on port ${port}`);
