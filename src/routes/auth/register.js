1709a1858060aefe17bf6d9feeaaa109bf476d2c
MHines10 <cd.mhines@gmail.com>
2023-01-31 13:35:12 -0500

    adding userSchema and model, add user to db on register

--- src/routes/auth/register.js
+++ src/routes/auth/register.js
@@ -1,4 +1,14 @@
+const { User } = require('../../models');
+
 module.exports = ((req, res) => {
     console.log(req.body);
-    res.send('Post Auth Router Register')
+    //first check to make sure the password and confirm pass are the same
+    if (req.body.password !== req.body.ConfirmPass){
+        res.send({error: "passwords do not match"});
+    }
+    // get data from request body
+    const { username, email, password } = req.body;
+    const user = new User({ username, email, password });
+    user.save();
+    res.send(`New User Created - ${user.username}`);
 })
