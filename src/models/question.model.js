8d66bf74dfe0c7eb7a3c39a758611efd19425833
MHines10 <cd.mhines@gmail.com>
2023-02-02 12:56:35 -0500

    added quiz and question models

---
+++ src/models/question.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const questionSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        correctAnswer: {
+            type: String,
+            required: true,
+        },
+        order: {
+            type: Number,
+            required: true,
+        },
+        quizId: {
+            type: Number,
+            required: true, 
+        }
+    },
+    { timestamps: true }
+)
+
+module.exports = new mongoose.model("Questions", questionSchema)
