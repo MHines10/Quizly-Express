c0557b3ec2016d9d6ca617105b79296ac03b5422
MHines10 <cd.mhines@gmail.com>
2023-02-03 15:48:40 -0500

    submitQuiz mutation with AnswerInputType

--- src/routes/quiz/index.js
+++ src/routes/quiz/index.js
@@ -2,7 +2,12 @@ const QuizDashboardRouter = require('express').Router();
 
 
 QuizDashboardRouter.route('/create')
-    .get(require('./editor'));
+    .get(require('./editor'))
+    .post(require('./create'))
+
+// slug after mains
+QuizDashboardRouter.route('/:slug')
+    .get(require('./view'))
 
 
 module.exports = QuizDashboardRouter;
